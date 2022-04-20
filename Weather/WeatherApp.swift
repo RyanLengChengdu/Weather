533752fee1862a1aa73030113b5629e2c06d473e
ryanleng <khalillan@hotmail.com>
2022-04-20 10:17:38 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 冷锐鹏 on 2022/4/20.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
